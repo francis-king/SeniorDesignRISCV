edac656651f0697b59286bb399a195dd3953d437
Amritchandar <66382601+Amritchandar@users.noreply.github.com>
2024-01-27 15:12:55 -0600

    added csr instruction

--- Source Files/csr_file.v
+++ Source Files/csr_file.v
@@ -1,9 +1,10 @@
 //Does it always write to the CSR? Added a valid bit. Can change later?
 module csr_file(
-    input [4:0] DR,
-    input [4:0] SR,
+    input [11:0] DR,
+    input [11:0] SR,
     input [63:0] DATA,
     input LD_REG,
+    input ST_REG,
     input CS,
     input [63:0] BASE_ADDRESS,
     input [63:0] CAUSE,
@@ -14,12 +15,22 @@ module csr_file(
     input CLK
 );
 
-reg [63:0] regFile [31:0];
+reg [63:0] regFile [4095:0];
 
+//csr file will load and store data in the same cycle
 always @(posedge CLK) begin
-    if (LD_REG) begin
-        regFile[DR] <= DATA;
+    if(CS)begin
+
     end
+    else begin
+        if (ST_REG)begin
+            OUT <= regFile[SR]
+        end
+        if (LD_REG) begin
+            regFile[DR] <= DATA;
+        end
+    end
+
 end
 assign OUT = regFile[SR];
 endmodule
