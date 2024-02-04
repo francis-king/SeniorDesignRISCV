eee62128bedf2e549617d0b5f91f3735208ae5ab
Amritchandar <66382601+Amritchandar@users.noreply.github.com>
2024-02-04 16:15:23 -0600

    top module is done

---
+++ Source Files/top.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/02/2024 12:55:36 PM
+// Design Name: 
+// Module Name: csr
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: This File holds the Control Status Registers for the core and handles
+// the hardware operations that occur when a context switch occurs (interrupt/exception).
+// The CSR file can also be written to and read from using the 6 csr instructions
+//  
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module top()
+
+endmodule
