ad89ab44b61750826e6713ebab626685d0aadffc
Wenyu Zhu <wenyuzhu02@utexas.edu>
2024-02-06 17:41:28 -0600

    Fetch Illegal Instruction

--- Source Files/fetch.v
+++ Source Files/fetch.v
@@ -11,7 +11,8 @@ module fetch (
     output reg [31:0] DE_IR,
     output reg DE_V,
 );
-
+`define opcode DE_IR[6:0]
+`define func3 DE_IR[14:12]
 reg [63:0] FE_PC;
 wire FE_LD_PC, FE_LD_DE;
 wire [63:0] FE_PC_input;
@@ -23,9 +24,9 @@ always @(posedge CLK) begin
         FE_PC <= FE_PC_input;
     end
     if (LD_DE) begin
-        DE_V = icache_r && !v_de_br_stall && !v_agex_br_stall && !v_mem_br_stall;;
-        DE_IR = FE_instruction;
-        DE_NPC = FE_PC + 4;
+        DE_V <= icache_r && !v_de_br_stall && !v_agex_br_stall && !v_mem_br_stall;;
+        DE_IR <= FE_instruction;
+        DE_NPC <= FE_PC + 4;
     end
 end
 
@@ -45,6 +46,8 @@ assign FE_LD_PC = (mem_stall || v_de_br_stall || v_agex_br_stall || (icache_r &&
 //     LD_DE = 0;
 // } else { LD_DE = 1; }
 assign FE_LD_DE = (mem_stall) ? 'd0 : 'd1;
+
+assign FE_illegal_instruction = ((`opcode == 7'b0110111) || (`opcode == 7'b0010111) || (`opcode == 7'b1101111) || ((`opcode == 7'b1100111) && (`func3 == 3'b000)) || (`opcode == 7'b1100011) || ((`opcode == 7'b0000011) && (`func3 != 3'b111)) || ((`opcode == 7'b0100011) && (DE_IR[14] == 1'b0)) || (`opcode == 7'b0010011) || (`opcode == 7'b0110011) || (`opcode == 7'b0001111) || (`opcode == 7'b1110011) || ((`opcode == 7'b0011011) && ((`func3 == 3'b001) || (`func3 == 3'b101) || (`func3 == 3'b000))) || ((`opcode == 7'b0111011) && ((`func3 == 3'b001) || (`func3 == 3'b101) || (`func3 == 3'b000))) || (`opcode == 7'b1110011) || (`opcode == 7'b0110011) || (`opcode == 7'b0111011)) ? 1'd0 : 1'd1;
 
 always@(*) begin
     case(MEM_PCMUX)
