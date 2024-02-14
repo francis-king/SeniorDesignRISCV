0800a018559d67cb143555afaa6a931633768c5f
Amritchandar <66382601+Amritchandar@users.noreply.github.com>
2024-02-14 15:17:48 -0600

    stuf

--- Source Files/top.v
+++ Source Files/top.v
@@ -26,6 +26,9 @@ module top(
 )
 //stall signals
 wire v_de_br_stall;
+wire mem_stall;
+wire wb_stall;
+
 
 //wires from FETCH stage
 wire f_iam;
@@ -57,6 +60,15 @@ wire [63:0] mem_ir_old;
 wire [4:0]  mem_drid;
 wire [63:0] mem_alu_result;
 wire        mem_stall;
+wire [63:0] mem_pc;
+wire [63:0] mem_alu_result;
+wire [31:0] mem_ir;
+wire [63:0] mem_sr2;
+wire [63:0] mem_sr1;
+wire        mem_v;
+wire [63:0] mem_csrfd;
+wire [63:0] mem_rfd;
+wire        mem_ecall;
 
 //wires from WRITEBACK stage
 wire [31:0] wb_ir;
@@ -97,35 +109,84 @@ decode decode_stage(
     .DE_NPC(de_npc),
     .DE_IR(de_ir),
     .DE_V(de_v),
-    WB_IR(wb_ir),
-    WB_CSRFD(wb_csrfd),
-    WB_DATA(wb_data),
-    MEM_ALU_RESULT(mem_alu_result),
-    WB_ALU_RESULT(wb_alu_result),
-    WB_MEM_RESULT(wb_mem_result),
-    WB_CS(wb_cs),
-    WB_CAUSE(wb_cause),
-    EXE_IR_OLD(exe_ir_old),
-    MEM_IR_OLD(mem_ir_old),
-    EXE_DRID(exe_drid),
-    MEM_DRID(mem_drid),
-    WB_ST_REG(wb_st_reg),
-    WB_ST_CSR(wb_st_csr),
-    WB_IR(wb_ir),
-    MEM_STALL(mem_stall),
-    CLK(CLK),
-    reset(RESET),
-    EXE_NPC(exe_npc),
-    EXE_CSFR(exe_csrfd),
-    EXE_ALU_ONE(exe_alu_one),
-    EXE_ALU_TWO(exe_alu_two),
-    EXE_IR(exe_ir),
-    EXE_V(exe_v),
-    EXE_ECALL(exe_ecall),
-    EXE_RFD(exe_rfd),
-    v_de_br_stall(v_de_br_stall),
-    DE_MTVEC(de_mtvec)
-)
+    .WB_IR(wb_ir),
+    .WB_CSRFD(wb_csrfd),
+    .WB_DATA(wb_data),
+    .MEM_ALU_RESULT(mem_alu_result),
+    .WB_ALU_RESULT(wb_alu_result),
+    .WB_MEM_RESULT(wb_mem_result),
+    .WB_CS(wb_cs),
+    .WB_CAUSE(wb_cause),
+    .EXE_IR_OLD(exe_ir_old),
+    .MEM_IR_OLD(mem_ir_old),
+    .EXE_DRID(exe_drid),
+    .MEM_DRID(mem_drid),
+    .WB_ST_REG(wb_st_reg),
+    .WB_ST_CSR(wb_st_csr),
+    .WB_IR(wb_ir),
+    .MEM_STALL(mem_stall),
+    .CLK(CLK),
+    .reset(RESET),
+    .EXE_NPC(exe_npc),
+    .EXE_CSFR(exe_csrfd),
+    .EXE_ALU_ONE(exe_alu_one),
+    .EXE_ALU_TWO(exe_alu_two),
+    .EXE_IR(exe_ir),
+    .EXE_V(exe_v),
+    .EXE_ECALL(exe_ecall),
+    .EXE_RFD(exe_rfd),
+    .v_de_br_stall(v_de_br_stall),
+    .DE_MTVEC(de_mtvec)
+);
+
+execute execute_stage(
+    .CLK(CLK),
+    .EXE_NPC(exe_npc),
+    .EXE_CSFR(exe_csrfd),
+    .EXE_ALU_ONE(exe_alu_one),
+    .EXE_ALU_TWO(exe_alu_two),
+    .EXE_IR(exe_ir),
+    .EXE_V(exe_v),
+    .EXE_ECALL(exe_ecall),
+    .EXE_RFD(exe_rfd),
+    .MEM_PC(mem_pc),
+    .MEM_ALU_RESULT(mem_alu_result),
+    .MEM_IR(mem_ir),
+    .MEM_SR2(mem_sr2),
+    .MEM_SR1(mem_sr1),
+    .MEM_V(mem_v),
+    .MEM_CSRFD(mem_csrfd),
+    .MEM_RFD(mem_rfd),
+    .MEM_stall(mem_stall),
+    .MEM_ECALL(mem_ecall)
+);
+
+memory memory_stage(
+    .CLK(CLK),
+    .WB_STALL(wb_stall),
+    .MEM_NPC(mem_n),
+    .MEM_CSRFD,
+    .MEM_ALU_RESULT,
+    .MEM_SR1,
+    .MEM_SR2,
+    .MEM_V,
+    .MEM_RFD,
+    .MEM_DRID,
+    .MEM_ECALL,
+    .WB_NPC,
+    .WB_CSRFD,
+    .WB_ALU_RESULT,
+    .WB_MEM_RESULT,
+    .WB_PC_MUX,
+    .WB_V,
+    .WB_RFD,
+    .WB_DRID,
+    .WB_ECALL,
+    .MEM_LAM,
+    .MEM_LAF,
+    .MEM_SAM,
+    .MEM_SAF,
+);
 
 
 
